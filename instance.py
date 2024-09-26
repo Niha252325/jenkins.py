ef1387bcfbafe4082a64048d7fdb88fb5c6cf298
Niha252325 <nihanaidu2517@gmail.com>
2024-09-26 23:57:15 +0530

    Create instance.py

---
+++ instance.py
@@ -0,0 +1,17 @@
+import boto3
+import sys
+
+region = sys.argv[1]
+access_key = sys.argv[2]
+secret_key = sys.argv[3]
+client = boto3.client('ec2', region_name=region,aws_access_key_id=access_key,aws_secret_access_key=secret_key)
+response = client.describe_instances(
+    Filters=[
+        {
+            'Name': 'instance-type',
+            'Values': [
+                't2.small',
+            ],
+        },
+    ],
+)
